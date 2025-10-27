5881a5108ba2e2b71e159d2fbc9522c74990e005
inchara-s-n <your-email-for-inchara-s-n>
2025-10-27 12:16:09 +0530

    Initial Commit

---
+++ C2TC__Training/src/com/tns/day1/DataTypes.java
@@ -0,0 +1,19 @@
+package com.tns.day1;
+
+public class DataTypes {
+
+
+
+	public static void main(String[] args) {
+
+       int a= 24;
+       int b= 13;
+       System.out.println("integer division :"  + a/b);
+       System.out.println("float division :" + (float)a/b);
+       System.out.println("double division :" + (double)a/b);
+
+	}
+
+
+
+}
---
+++ C2TC__Training/src/com/tns/day1/TypeCasting.java
@@ -0,0 +1,26 @@
+package com.tns.day1;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		//Implicit type casting
+		
+	int a = 30;
+	float b = a;
+	System.out.println(b);
+	
+	char aa = 'A';
+	int bb = aa;
+	System.out.println(bb);
+	//explicit type conversion
+	
+	double c = 9.997968585856;
+	int cc = (int) c;
+	System.out.println(cc);
+				
+
+
+
+	}
+
+}
